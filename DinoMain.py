f44bf59be010a503bd3c80506da1c0ce7cec1bf7
asarenski <asarenski@gmail.com>
2014-05-22 13:53:13 -0400

    Newest

--- DinoMain.py
+++ DinoMain.py
@@ -1,6 +1,12 @@
 __author__ = 'adam'
 
-from Tkinter import *
+'''
+Dinomuncher USA!
+'''
+
+from Tkinter import * # tkinter is a GUI writing module that can also be used to write games
+from PIL import Image
+import sys
 
 class DinoFrame(Frame):
     def __init__(self, master=None, rows=8, columns=8, size=96, color1='old lace', color2="pale turquoise"):
@@ -24,11 +30,34 @@ class DinoFrame(Frame):
                                 width=canv_width, height=canv_height, background="white")
         self.canvas.pack(side="top", fill="both", expand=True, padx=2, pady=2)
 
-        # binds self.refresh to teh configure event
+        # binds self.refresh to the configure event
         self.canvas.bind("<Configure>", self.refresh)
 
         # initialize widgets
         self.createWidgets()
+
+    def piece(self, name, image, row=0, column=0):
+    # Creates an image and puts it on the board. Also calls placepiece, which puts it in the pieces dictionary.
+        self.image = PhotoImage(file=image) # takes the string of the .gif
+        self.canvas.create_image(0,0, image = self.image, tags=(name, "piece"), anchor="c")
+        self.placepiece(name, row, column) # calls the placepiece function, which places it in coordinates
+
+    def placepiece(self, name, row, column):
+        self.pieces[name] = (row, column)
+        x0 = (column * self.size) + int(self.size/2)
+        y0 = (row * self.size) + int(self.size/2)
+        self.canvas.coords(name, x0, y0)
+
+    '''
+    def resize_piece(self):
+        # somehow have to capture the original size and compare it to the new sizing event
+        scale_w = self.size/old_width
+        scale_h = self.size/old_height
+        PhotoImage.zoom(scale_w, scale_h)
+
+        old_width = self.size
+        old_height = self.size
+    '''
 
     # say_hi widget command
     def say_hi(self):
@@ -72,19 +101,47 @@ class DinoFrame(Frame):
 
         # use this when you start to put characters on the board
         # need to do the same thing with the numbers that we put on the board
+
         for name in self.pieces:
             self.placepiece(name, self.pieces[name][0], self.pieces[name][1])
-        self.canvas.tag_raise("piece")
-        self.canvas.tag_lower("square")
+            # places the piece back where it was on the canvas
 
+            self.canvas.tag_raise("piece")
+            self.canvas.tag_lower("square")
         # put code here for numbers
 
-def start_app():
+    #controls
+    def leftkey(self,event):
+        print "monkeys"
+    def rightkey(self,event):
+        print "are"
+    def upkey(self,event):
+        print "the"
+    def downkey(self,event):
+        print "very"
+    def spacekey(self,event):
+        print "cool"
+
+#end
+
+def main():
     root = Tk() #creates the tk
     root.title("Dinomuncher USA!")
     app = DinoFrame(master=root)
     app.pack(side="top", fill="both", expand="true", padx=4, pady=4)
+
+    # calling the piece method and initializes player1
+    player1 = "rex_skull1.gif"
+    app.piece("player1",player1,0,0)
+
+    # key bindings
+    root.bind('<Escape>', lambda e: root.quit())
+    root.bind('<Left>',app.leftkey)
+    root.bind('<Right>',app.rightkey)
+    root.bind('<Up>',app.upkey)
+    root.bind('<Down>',app.downkey)
+    root.bind('<Space>',app.leftkey)
+
     root.mainloop()
     root.destroy()
-
-start_app()
+main()
--- Main.py
+++
@@ -1,8 +0,0 @@
-'''
-Purpose of main is to call the other stuff
-'''
-
-
-#from Graphics1 import start_app
-
-#start_app()

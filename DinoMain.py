9c50f3b9eab32ee3e64b8a4f70bbd6ccfeef3f7d
asarenski <asarenski@gmail.com>
2014-05-23 20:59:03 -0400

    Saved before window resizing removed

--- DinoMain.py
+++ DinoMain.py
@@ -10,8 +10,9 @@ import sys
 
 class DinoFrame(Frame):
     def __init__(self, master=None, rows=8, columns=8, size=96, color1='old lace', color2="pale turquoise"):
-        #size is the size of a square in pixels
+        # size is the size of a square in pixels
 
+        # initialize variables
         self.rows = rows
         self.columns = columns
         self.size = size
@@ -38,36 +39,7 @@ class DinoFrame(Frame):
         # initialize widgets
         self.createWidgets()
 
-    def piece(self, name, image, row=0, column=0):
-    # Creates an image and puts it on the board. Also calls placepiece, which puts it in the pieces dictionary.
-        self.image = PhotoImage(file=image) # takes the string of the .gif
-        self.canvas.create_image(0,0, image = self.image, tags=(name, "piece"), anchor="c")
-        self.placepiece(name, row, column) # calls the placepiece function, which places it in coordinates
-
-    def placepiece(self, name, row, column):
-        self.pieces[name] = [row, column] #create a row, column list.. in previous code this was tuple
-        # technically x0 should be using column. instead just imagine for the coordinate system
-        # that the row just means x_i . This is so that it plays nicely with refresh event for board
-        x0 = (row * self.size) + int(self.size/2)
-        y0 = (column * self.size) + int(self.size/2)
-        self.canvas.coords(name, x0, y0)
-
-    '''
-    def resize_piece(self):
-        # somehow have to capture the original size and compare it to the new sizing event
-        scale_w = self.size/old_width
-        scale_h = self.size/old_height
-        PhotoImage.zoom(scale_w, scale_h)
-
-        old_width = self.size
-        old_height = self.size
-    '''
-
-    # print_coord widget command
-    def print_coord(self):
-        print self.pieces['player1']
-
-    # initialize widgets
+    # function for pushbutton widgets
     def createWidgets(self):
 
         # quit button
@@ -78,11 +50,56 @@ class DinoFrame(Frame):
         self.QUIT["command"] =  self.quit
         self.QUIT.pack({"side": "left"})
 
-        # coordinates button
+        # print coordinates button
         self.hi_there = Button(self)
         self.hi_there["text"] = "Print_Coord",
         self.hi_there["command"] = self.print_coord
         self.hi_there.pack({"side": "left"})
+
+        # function print_coord widget command
+    def print_coord(self):
+        print self.pieces['player1']
+
+    # function to put the image of the piece on the board
+    def piece(self, name, image, row=0, column=0):
+    # Also calls placepiece, which puts it in the pieces dictionary.
+        self.image = PhotoImage(file=image) # takes the string of the .gif
+        self.canvas.create_image(0,0, image = self.image, tags=(name, "piece"), anchor="c") # c for center
+        self.placepiece(name, row, column) # calls the placepiece function, which places it in coordinates
+
+    # function to track coordinates for piece in dictionary 'pieces'
+    def placepiece(self, name, row, column):
+        self.pieces[name] = [row, column] #create a row, column list.. in previous code this was tuple
+        # technically x0 should be using column. instead just imagine for the coordinate system
+        # that the row just means x_i . This is so that it plays nicely with refresh event for board
+        x0 = (row * self.size) + int(self.size/2)
+        y0 = (column * self.size) + int(self.size/2)
+        self.canvas.coords(name, x0, y0)
+
+    # function to draw the numbers on the board
+    def shownumber(self, act_number, row, column):
+        self.numbers[act_number] = [row , column]
+        text1 = act_number
+
+        # loop draws the numbers
+        for row in range(8):
+            for col in range(8):
+                x1 = (col * self.size) + int(self.size/2)-3
+                y1 = (row * self.size) + int(self.size/2)-3
+                self.canvas.create_text(x1,y1, font =("Times", "24", "bold"), text=text1)
+
+
+    '''
+    # function for future use. to resize 'player1' when window resized
+    def resize_piece(self):
+        # somehow have to capture the original size and compare it to the new sizing event
+        scale_w = self.size/old_width
+        scale_h = self.size/old_height
+        PhotoImage.zoom(scale_w, scale_h)
+
+        old_width = self.size
+        old_height = self.size
+    '''
 
     # Redraws the board, such as when the window is resized
     def refresh(self, event):
@@ -101,7 +118,6 @@ class DinoFrame(Frame):
                 self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill=color, tags="square")
                 color = self.color1 if color == self.color2 else self.color2
 
-        # use this when you start to put characters on the board
         # need to do the same thing with the numbers that we put on the board
         for name in self.pieces:
             self.placepiece(name, self.pieces[name][0], self.pieces[name][1])
@@ -109,7 +125,10 @@ class DinoFrame(Frame):
 
             self.canvas.tag_raise("piece") # tag raise makes "piece" the top layer
             self.canvas.tag_lower("square")
-        # put code here for numbers
+
+        # redraw the numbers
+        for act_number in self.numbers:
+            self.shownumber(act_number,0,0)
 
     #controls
     # these events will move the player 96 pixels by default
@@ -144,26 +163,30 @@ class DinoFrame(Frame):
     def spacebar(self,event):
         print "best"
 
-#end
+# end of DinoFrame
 
 def main():
     root = Tk() #creates the tk
     root.title("Dinomuncher USA!")
-    app = DinoFrame(master=root)
-    app.pack(side="top", fill="both", expand="true", padx=4, pady=4)
+    app = DinoFrame(master=root) # initialize the application
+    # app.master.maxsize(736,768) # sets the max size of the frame
+    app.pack(side="top", fill="both", expand="true", padx=4, pady=4) # pad gives window a little extra room
 
     # calling the piece method and initializes player1
-    player1 = "rex_skull2.gif"
-    app.piece("player1",player1,0,0)
+    player1 = "rex_skull2.gif" # sets the variable to a string
+    app.piece("player1",player1,0,0) # calls the piece method arguments are (name, image, row, column)
+
+    #call numbers
+    app.shownumber(36,0,0)
 
     # key bindings
-    root.bind('<Escape>', lambda e: root.quit())
-    root.bind('<Left>',app.leftkey)
+    root.bind('<Escape>', lambda e: root.quit()) # lambda allows you to write user input functions
+    root.bind('<Left>',app.leftkey) # these call the keys. one could also write this with %s and a dictionary
     root.bind('<Right>',app.rightkey)
     root.bind('<Up>',app.upkey)
     root.bind('<Down>',app.downkey)
     root.bind('<space>',app.spacebar)
 
-    root.mainloop()
+    root.mainloop() # begins the application
     root.destroy()
 main()

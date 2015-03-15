d39674656aadef93b3a761de336c781653964fd9
asarenski <asarenski@gmail.com>
2015-03-15 13:44:02 -0400

    setting up gh-pages

---
+++ js/dino.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+            $('#example').popover({
+                html: true,
+                trigger: "click",
+                placement: "bottom",
+                title: "Bryan Oakley's Submission"
+            });
+            $('#example-2').popover({
+                html: true,
+                trigger: "click",
+                placement: "bottom",
+                title: "Bryan Oakley's Submission-2"
+            });
+        });
